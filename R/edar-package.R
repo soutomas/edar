0f1631c2f6ffe29de3c177977ccefd42de3efa52
Sou <souto1@novartis.net>
2025-09-26 11:12:58 +0200

    First build

---
+++ R/edar-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom magrittr %>%
+#' @importFrom magrittr extract
+#' @importFrom magrittr extract2
+## usethis namespace: end
+NULL
